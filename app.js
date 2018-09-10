9595235af013d37c39b2b83a105c3ada0071487f
Sureshraja <sureshraja.sun@gmail.com>
2018-09-10 12:00:26 +0800

    Added the EJS support as a view engine

--- app.js
+++ app.js
@@ -13,10 +13,12 @@ app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dis
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')) )
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')) )
 
+app.set('views', './src/views' );
+app.set('view engine', 'ejs');
 
 
-app.get('/', function (req, res){
-    res.send('Hello from my library');
+app.get('/', (req, res) => {
+    res.render('index', { list: ['a', 'b'], title: 'Library'});
 });
 
 app.get('/index', function (req, res){
