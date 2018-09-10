0b28c5f0ab15d33e29eadcd8676b2812b0fdb629
Sureshraja <sureshraja.sun@gmail.com>
2018-09-10 13:09:05 +0800

    Added the navigation pages with default data

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var path = require('path');
 
 
 var app = express();
+var port = process.env.PORT || 3000;
+var bootRouter = express.Router();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname,'/public/')))
@@ -16,22 +18,70 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist'))
 app.set('views', './src/views' );
 app.set('view engine', 'ejs');
 
+var books = [
+    { 
+        title: 'Power reading',
+        genre: 'Self Improvement',
+        author: 'Macmillan',
+        read: 'fale'
+    },
+    { 
+        title: 'Mac notes',
+        genre: 'Technology',
+        author: 'Kumar',
+        read: 'fale'
+    },
+    { 
+        title: 'Art of combat',
+        genre: 'Self Defence',
+        author: 'Macmillan',
+        read: 'fale'
+    },
+    { 
+        title: 'Study plus',
+        genre: 'Education',
+        author: 'Paul',
+        read: 'fale'
+    }
+];
+bootRouter.route('/')
+    .get((req, res) => {
+        res.render('books',
+        { 
+            nav:[
+                    {title: 'Books', link : '/books'},
+                    {title: 'Author', link : '/author'}
+                ], 
+            title: 'Library',
+            books : books
+        });
+    });
+
+bootRouter.route('/single')
+    .get((req, res) => {
+        res.send("Hello Single Book");
+    });
+
+
+app.use('/books', bootRouter);
 
 app.get('/', (req, res) => {
     res.render('index', 
         { 
             nav:[
-                    {title: 'Book', link : '/book'},
+                    {title: 'Book', link : '/books'},
                     {title: 'Author', link : '/author'}
                 ], 
             title: 'Library'
         });
 });
 
+
+
 app.get('/index', function (req, res){
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 app.listen(3000, function (){
-    debug(`Listen on port ${chalk.red("3000")}`);
+    debug(`Listen on port ${chalk.red(port)}`);
 });   
